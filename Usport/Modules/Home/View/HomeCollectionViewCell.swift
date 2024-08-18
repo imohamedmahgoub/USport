69979a2e48223bb3aa6cbfff0a67b668cf6b4181
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-08-18 13:10:58 +0300

    adding radious to the sport image

--- Usport/Modules/Home/View/HomeCollectionViewCell.swift
+++ Usport/Modules/Home/View/HomeCollectionViewCell.swift
@@ -11,4 +11,13 @@ class HomeCollectionViewCell: UICollectionViewCell {
     @IBOutlet var sportLbl: UILabel!
     @IBOutlet var sportImg: UIImageView!
     
+    override func awakeFromNib() {
+
+       self.layoutIfNeeded()
+        layer.cornerRadius = self.frame.height / 8
+        layer.masksToBounds = true
+        sportImg.contentMode = .scaleAspectFill
+
+
+      }
 }
